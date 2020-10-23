bb368cfe1cbc96573fbdb27b78da53547b6820c9
Bruno jose da Silva <brunofat@Mac-mini-de-Mac.local>
2020-10-23 17:05:50 -0300

    enviando para o git

---
+++ plugin-lgpd.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Plugin LGPD
+Plugin URI: https://wordpress.org/plugins/accelerated-mobile-pages/
+Description: Criação de Plugin para aceitação de termos e aceite (LGPD)
+Version: 1.0
+Author: Bruno José
+
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit; ?>
+
+
+<h3>Plugin LGPD</h3>
+
+
